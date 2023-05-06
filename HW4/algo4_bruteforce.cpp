e676ef3c53a3bcf632b559cd2dad6b9b66a1c8f2
Wennyyyy <lingrace0324@gmail.com>
2023-05-06 13:59:16 +0800

    * Add Makefile * Update alogo4_bruteforce ** cin to fin

--- HW4/algo4_bruteforce.cpp
+++ HW4/algo4_bruteforce.cpp
@@ -1,40 +1,42 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
 #include <time.h>
 #include <windows.h>
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <vector>
 using namespace std;
 #define INF 5000
 // nmax:15 Cmax:100
 vector<int> solution;
 
-int TSP(vector<vector<int>> &map, int n, int s){
+int TSP(vector<vector<int>> &map, int n, int s) {
     vector<int> possibleWay;
     vector<int> vertexes;
     int minPath = INF;
     int currentPath = 0, vtx = s;
 
-    for(int i = 0; i < n; i++){
-        if( i != s){
+    for (int i = 0; i < n; i++) {
+        if (i != s) {
             // store other vertexes except for city 1
             vertexes.push_back(i);
         }
     }
 
-    do{
+    do {
         // clear possible path
         possibleWay.erase(possibleWay.begin(), possibleWay.end());
         currentPath = 0;
         vtx = s;
-        for(int i = 0; i < vertexes.size(); i++){
+        for (int i = 0; i < vertexes.size(); i++) {
             currentPath += map[vtx][vertexes[i]];
             vtx = vertexes[i];
             possibleWay.push_back(vtx);
         }
         currentPath += map[vtx][s];
-        if(minPath > currentPath){
+        if (minPath > currentPath) {
             solution.erase(solution.begin(), solution.end());
-            for(int i = 0; i < possibleWay.size(); i++){
+            for (int i = 0; i < possibleWay.size(); i++) {
                 solution.push_back(possibleWay[i]);
             }
             minPath = currentPath;
@@ -51,34 +53,49 @@ int TSP(vector<vector<int>> &map, int n, int s){
             }*/
         }
         // do for all permutation
-    }while(next_permutation(vertexes.begin(), vertexes.end()));
+    } while (next_permutation(vertexes.begin(), vertexes.end()));
     return minPath;
- }
-int main(int argc, char *argv[]){
+}
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cout << "ERROR!!ROAR!!" << endl;
+        return 1;
+    }
+    ifstream fin;
+    ofstream fout;
+    fin.open(argv[1], ios::in);
+    if (!fin.is_open()) {
+        cout << "Failed to open" << argv[1] << "!!! U idiot sandwitch" << endl;
+        return 1;
+    }
+
     int nCities, solutionCost = 0;
-    cin >> nCities;
+    fin >> nCities;
     vector<vector<int>> Cost(nCities, vector<int>(nCities));
-    for(int i = 0; i < nCities; i++){
-        for(int j = 0; j < nCities; j++){
-            cin >> Cost[i][j];
+    for (int i = 0; i < nCities; i++) {
+        for (int j = 0; j < nCities; j++) {
+            fin >> Cost[i][j];
         }
     }
-    clock_t startTime,endTime;
+    fin.close();
+
+    clock_t startTime, endTime;
     int s = 0;
     startTime = clock();
     Sleep(1000);
     solutionCost = TSP(Cost, nCities, s);
     endTime = clock();
-    cout << "The shortest Hamiltonian cycle:" << endl <<"1 ";
-    for(int i = 0; i < solution.size(); i++){
+    cout << "The shortest Hamiltonian cycle:" << endl
+         << "1 ";
+    for (int i = 0; i < solution.size(); i++) {
         cout << solution[i] + 1;
-        if( i < solution.size() - 1){
-            cout << " " ;
-        }else{
-            cout <<" 1" <<endl;
+        if (i < solution.size() - 1) {
+            cout << " ";
+        } else {
+            cout << " 1" << endl;
         }
     }
     cout << "Total cost: " << solutionCost << endl;
-    cout << "Time: " << ((double)(endTime - startTime))/ CLOCKS_PER_SEC - 1<<" s\n";
+    cout << "Time: " << ((double)(endTime - startTime)) / CLOCKS_PER_SEC - 1 << " s\n";
     return 0;
 }
